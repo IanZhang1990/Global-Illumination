ac971c50293f7c3452da6bd13e6fd7db97509605
Yinan Zhang <ian.zhang1990@hotmail.com>
2014-02-22 17:33:59 -0500

    object class

---
+++ source/Rendart/Core/Object.h
@@ -0,0 +1,21 @@
+
+#ifndef CORE_OBJECT
+#define CORE_OBJECT
+
+namespace Core
+{
+	/***************************************************************
+	Object class.
+	It represents an object in the scene. 
+	***************************************************************/
+	class Object
+	{
+	public:
+
+	protected:
+
+
+	};
+}
+
+#endif
